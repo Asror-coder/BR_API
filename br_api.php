8c23450e0df786ec9b73a9f5ccc8f36480abf9a1
Asror-coder <60745191+Asror-coder@users.noreply.github.com>
2021-03-02 13:00:03 +0100

    Update br_api.php

--- br_api.php
+++ br_api.php
@@ -17,13 +17,14 @@
             CURLOPT_TIMEOUT => 30,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => "{\"query\":\"query {\\n  court_reservations(dates: [\\\"2021-02-05\\\"], resources: [1999]) {\\n     total\\n          nodes {\\n            resource {\\n              name\\n            }\\n            start_time\\n            end_time\\n          }\\n        }\\n      }\"}",
+            CURLOPT_POSTFIELDS => "{\"query\":\"mutation CreateReservation ($input : CreateReservationInput!) {\\n  createReservation(input: $input)\\n  {\\n  \\tid\\n  }\\n}\\n\",\"variables\":{\"input\":{\"resource_id\":1999,\"date\":\"2021-02-28\",\"start_time\":\"15:00\",\"end_time\":\"16:00\"}}}",
             CURLOPT_HTTPHEADER => [ 
-                "Content-Type: application/json" 
+                "Content-Type: application/json",
+                "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"
             ],
             CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
             CURLOPT_USERPWD => "API-BR-debook:APIkasdfhoisfh340rtu3fnr3ewfhr3gewy8h03g",
-            ]);
+            ]); 
 
             $response = curl_exec($curl);
             $err = curl_error($curl);
